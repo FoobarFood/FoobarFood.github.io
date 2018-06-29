0dc2a66704808c120476e479f067ad7c2259dffc
Malvika Patrachari <malvikapatrachari@Malvikas-MacBook-Pro-2.local>
2018-06-29 12:08:17 -0400

    Added Bing stuff

--- assets/javascript/weatherAPI.js
+++ assets/javascript/weatherAPI.js
@@ -1,56 +1,44 @@
-//API Key
+// bingAPI.getData('1600 pennsylvania ave washington d.c.')
+//   .then(function(response){
+//     var coordinates = bingAPI.getCoordinates(response);
 
-var APIKey = "2d1b540b6f1afe91eb3a62874c28c56f";
+//     weatherAPI.weatherData(coordinates)
+//         .then(function(response){
+//             var weatherDetails = weatherAPI.getWeatherDetails(response);
+//             console.log(weatherDetails);
+//             // change the Dom
+//         });
 
-var zipCode = "07002"
+//   });
 
-var city = "New York"
+var weatherAPI = (function(){
+    var APIKey = "2d1b540b6f1afe91eb3a62874c28c56f";
 
-var lat = 51;
+    function queryURL(latlonArray) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + latlonArray[0] + "&lon=" + latlonArray[1] + "&units=imperial&appid="  + APIKey;
+        return queryURL;
+    }
 
-var lon= 3;
+    function weatherData(coordinates) {
+        var url = queryURL(coordinates);
+        return $.ajax({
+            url: url,
+            method: "GET"
+        })
+    }
 
-var latlon = [-51.789, -3.778];
+    function getWeatherDetails(response) {
+        return {
+            temperature: response.main.temp,
+            description: response.weather[0].main
+        };
+    }
 
-console.log("The latitude and longitude: "+ "latitude: " + latlon[0] + "longitude" + latlon[1]);
-
-//URL to query the database
-
-//var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + city + ",us&units=imperial&appid=" + APIKey;
-
-//var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + zipCode + ",us&units=imperial&appid=" + APIKey;
-
-//get the weather based on lattitude and longitude 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + latlon[0] + "&lon=" + latlon[1] + "&units=imperial&appid="  + APIKey;
-
-//+ ",us&units=imperial&appid="
-
+    return {
+        queryURL: queryURL,
+        weatherData: weatherData,
+        getWeatherDetails: getWeatherDetails
+    }
+})();
 
 
-//Run AJAX call 
-
-$.ajax({
-url: queryURL,
-method: "GET"
-})
-//store retreived data in the response object 
-
-.then(function(response){
-
-//Log the url
-console.log(queryURL);
-
-//Log the response object
-console.log(response); 
-
-//Log the response of the temperature
-console.log("The temperature is " + response.main.temp);
-
-//Log the response of the description for the day
-console.log(response.weather[0].main);
-
-//Content to HTML
-$("#fooString").append("The temperature is: " + response.main.temp + "<br>"); 
-$("#fooString").append("Weather conditions are: "+response.weather[0].main);
-
-})
