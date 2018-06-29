6c6aae574c4ddfd5c415c3869dfa4dd70e9e2a39
Malvika Patrachari <malvikapatrachari@Malvikas-MacBook-Pro-2.local>
2018-06-29 11:19:02 -0400

    added cordinates

--- assets/javascript/weatherAPI.js
+++ assets/javascript/weatherAPI.js
@@ -6,11 +6,24 @@ var zipCode = "07002"
 
 var city = "New York"
 
+var lat = 51;
+
+var lon= 3;
+
+var latlon = [-51.789, -3.778];
+
+console.log("The latitude and longitude: "+ "latitude: " + latlon[0] + "longitude" + latlon[1]);
+
 //URL to query the database
 
 //var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + city + ",us&units=imperial&appid=" + APIKey;
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + zipCode + ",us&units=imperial&appid=" + APIKey;
+//var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + zipCode + ",us&units=imperial&appid=" + APIKey;
+
+//get the weather based on lattitude and longitude 
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + latlon[0] + "&lon=" + latlon[1] + "&units=imperial&appid="  + APIKey;
+
+//+ ",us&units=imperial&appid="
 
 
 
@@ -37,6 +50,7 @@ console.log("The temperature is " + response.main.temp);
 console.log(response.weather[0].main);
 
 //Content to HTML
-$(".weatherContent").append("")
+$("#fooString").append("The temperature is: " + response.main.temp + "<br>"); 
+$("#fooString").append("Weather conditions are: "+response.weather[0].main);
 
 })
