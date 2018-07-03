31f659422e7e2dd2de7f8642cc5b7107ae891599
Faolun <james.coatesiii@gmail.com>
2018-07-03 11:55:53 -0400

    added foostring text to click element with updated clean name API for city name

--- assets/javascript/clickListeners.js
+++ assets/javascript/clickListeners.js
@@ -7,15 +7,17 @@ clickListeners.locator = function() {
     e.preventDefault();
 
     var locationInput = $('#location-input').val();
-      
+  
     bingAPI.getData(locationInput).then(function(response){
       var coordinates = bingAPI.getCoordinates(response);
+      var cleanName = bingAPI.getCleanName(response);
 
-      weatherAPI.weatherData(coordinates).then(function(response){
+           weatherAPI.weatherData(coordinates).then(function(response){
           var weatherDetails = weatherAPI.getWeatherDetails(response);
 
           console.log(weatherDetails);
           // TODO: change the DOM
+          $("#fooString").text(`Today in ${cleanName}, it is ${weatherDetails.temperature} with ${weatherDetails.description}. The perfect day for...`)
       });
     });
   });
