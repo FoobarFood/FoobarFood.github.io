2073a1bab76a6a71cd41d9c061de0b19c344cc92
Anthony Sin <anthony.t.sin@gmail.com>
2018-07-04 01:32:31 -0400

    additional fns to firebaseUtility

--- assets/javascript/firebaseUtility.js
+++ assets/javascript/firebaseUtility.js
@@ -1,6 +1,36 @@
-var firebaseUtility = {}
+var firebaseUtility = {};
 
 firebaseUtility.getFoods = function() {
     return firebase.database().ref('foods').once('value');
 }
 
+/*===== adds a food to foods in firebase database
+foodKey is string: Apple_Pie 
+foodValue is object: {
+  Meal: 'Snack',
+  TempNumber: 110,
+  WeatherNumber: 200   
+}
+*/
+firebaseUtility.addFood = function(foodKey, foodValue) {
+    firebase.database().ref('foods').child(foodKey).set(foodValue);
+}
+
+/*===== adds a recipeId to favorites in firebase database
+if recipeId does not exist in favorites, add it and set value to 1
+else increase value by 1
+*/
+firebaseUtility.addFavoriteRecipe = function(recipeId) {
+    firebase.database().ref('favorites').child(recipeId).once('value').then(function(snapshot){ 
+       if (snapshot.exists()) {
+           var favoritesCount = snapshot.val() + 1;
+           firebase.database().ref('favorites').child(recipeId).set(favoritesCount);
+       } else {
+           firebase.database().ref('favorites').child(recipeId).set(1);
+       };
+    });
+}
+
+firebaseUtility.getFavorites = function(limit) {
+    return firebase.database().ref('favorites').once('value');
+}
